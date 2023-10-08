d160550c95ad86cad58f16aacf667a0933873ab6
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-10-08 22:42:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    unsigned int x;
-    int b;
+int main(int argc, char *argv[]){
+    int sec, hour, min, sec2;
 
-    printf("input a number : ");
-    scanf("%ui", &x);
+    printf("input the second : ");
+    scanf("%i", &sec);
 
-    for (b = 0; x != 0; x >>= 1)
-    {
-        if (x & 1)
-        {
-            b++;
-        }
-    }
+    hour = sec / 60 / 60;
+    min = (sec - hour * 3600) / 60;
+    sec2 = sec % 60;
 
-    printf("The result is : %i \n", b);
-    
+    printf("the time is %d : %d : %d", hour, min, sec2);
     return 0;
 }
